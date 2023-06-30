99eec40559e611977971c7bc734d48cea17e71b7
River <hom0906@naver.com>
2023-06-30 16:28:55 +0900

    이벤트 & 메세지 시스템 원

--- WindowsMain/WinMain.cpp
+++ WindowsMain/WinMain.cpp
@@ -97,7 +97,144 @@
 	#. < DefWindowProc > : 기본 윈도우 프로시져로 일반적인 윈도우 기능을 대신 해준다.
 */
 
+//#include <Windows.h>
+//
+//const wchar_t gClassName[]{ L"MyWindowClass" };
+//
+//LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+//
+//int WINAPI WinMain(
+//	_In_ HINSTANCE hInstance,
+//	_In_opt_ HINSTANCE hPrevInstance,
+//	_In_ LPSTR lpCmdLine,
+//	_In_ int nShowCmd)
+//{
+//// #1. < 윈도우 클래스 생성 > : 윈도우 클래스라는 틀을 만들어 놓는다.
+////		=> 윈도우의 모양, 기능들을 묶어놓은 개념 집합
+//	WNDCLASSEX wc;
+//	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+//
+//	wc.style = CS_HREDRAW | CS_VREDRAW;
+//	wc.lpszClassName = gClassName;
+//	wc.hInstance = hInstance;
+//	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+//	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+//	wc.lpfnWndProc = WindowProc;
+//	wc.cbSize = sizeof(WNDCLASSEX);
+//
+//// #1-1. < 윈도우 클래스 등록 >
+////		=> 등록된 윈도우 클래스를 통해 윈도우를 찍어낼 수 있다.
+//	if (!RegisterClassEx(&wc))
+//	{
+//		MessageBox(nullptr, L"Failed to register class!", L"Error", MB_OK);
+//		return 0;
+//	}
+//
+//// #2. 윈도우 생성
+//	HWND hWnd;
+//	hWnd = CreateWindowEx(
+//		NULL,
+//		gClassName,
+//		L"Hellow Window",
+//		WS_OVERLAPPEDWINDOW,
+//		0, 0, 640, 480,
+//		nullptr, nullptr,
+//		hInstance,
+//		nullptr
+//	);
+//
+//// #2-1. 윈도우 생성 확인
+////		=> CreateWindowEx함수를 통해 윈도우 포인터를 받지 못하였을 경우
+//	if (hWnd == nullptr)
+//	{
+//		MessageBox(nullptr, L"Failed to create Window!", L"Error", MB_OK);
+//		return 0;
+//	}
+//
+//// #2-2. 윈도우가 정상적으로 만들어 졌을 경우
+////		=> 화면에 윈도우를 보여주고 윈도우를 갱신( 다시 그리기 )해준다.
+//	ShowWindow(hWnd, nShowCmd);
+//	UpdateWindow(hWnd);
+//
+//// #3. 메시지 루프
+////		=> 운영체제에 메세지가 있는지 물어보고 있으면 가져온다.
+////		=> 메세지가 없을 때까지 이 과정을 반복한다.
+//	MSG msg;
+//	while (GetMessage(&msg, nullptr, 0, 0))
+//	{
+//		TranslateMessage(&msg);
+//		DispatchMessage(&msg);
+//	}
+//
+//// #3-1. 프로그램 종료
+////		=> wParam : 현재 앱의 상태를 담고있는 포인터
+//	return static_cast<int>(msg.wParam);
+//}
+//
+//LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//	switch (message)
+//	{
+//		case WM_CLOSE:
+//			DestroyWindow(hWnd);
+//			break;
+//		case WM_DESTROY:
+//			PostQuitMessage(0);
+//			break;
+//		default:
+//			return DefWindowProc(hWnd, message, wParam, lParam);
+//			break;
+//	}
+//	return 0;
+//}
+
+/* ------ < Win32 Message&Event > ----- */
+
+/*
+< 윈도우OS의 작동 원리 >
+	#. 윈도우는 여러 개의 응용프로그램들이 한꺼번에 실행된다.
+	#. 이런 멀티태스킹 환경을 위해 개발한 것이 이벤트 메시지 시스템이다.
+	#. 먼저 키보드 or 마우스를 통한 입력을 '입력 큐'라는 큐 공간에 저장한다.
+	#. 그리고 운영체제 윈도우 관리자가 실행되는 입력만 차례대로 뽑아낸다.
+	#. 실행중인 응용프로그램에 차례대로 전달한다.
+	#. 정리하자면 운영체제에서 조작에 필요한 정보들을 받아서 실행중인 응용프로그램에게 각각 메시지를 보내주고
+	#. 응용프로그램은 그 메시지를 받아와서 필요한 처리를 하는 방식이다.
+	#. 이때 보내는 측에서 보면 메시지를 보낸다고 하며, 받는 측에서는 이벤트가 발생했다고 판단한다.
+
+< Message&Event > : 함수는 프로그래머가 만들지만 호출은 운영체제가 한다.
+	#. 함수 포인터를 운영체제에 넘겨준다.
+	#. 입력에 따른 이벤트가 발생할때 마다 CALLBACK이 발생하며 함수를 호출한다.
+		#. 이벤트( CALLBACK ) : 함수를 등록해 놓고 특정 상황에 함수를 대신 호출해줌
+		#. 메시지 : 여러개의 응용 프로그래밍에 전달하기 위해서 '누구 한테 전달할 꺼임?'을 정하는 것
+
+< MSG구조체의 형태 >
+	#. < HWND hwnd; > : 윈도우 핸들
+	#. < UINT message; > : 메세지의 유형을 나타내는 정수값
+		=> WM_( 윈도우 메세지 ) : 비트 플래그 형식으로 정의 되어 있는 메세지를 switch문에서 호출할 수 있다.
+	#. < WPARAM wParam; > : 메세지에 대한 추가 정보
+		=> WORD : 레지스터의 단위를 WORD라고 표현한다.
+			=> Register 명령 기억 : 단어 단위로 기억( 어쎔블리어 )
+			=> 16비트 컴퓨터에서 1WORD는 16bit이다.
+			=> 32비트 컴퓨터에서 1WORD는 32bit이다.
+	#. < LPARAM lParam; > : 메세지에 대한 추가 정보
+		=> LONG : WPARAM이 담지 못하는 좀더 큰 것을 담기 위한 더 큰 단위
+			#. WPARAM은 WORD타입의 Parameter라고 할 수 있고
+			#. LPARAM은 LONG타입의 Parameter라고 할 수 있다.
+		#. 그런데 현재는 WPARAM과 LPARAM의 크기가 같다. 다만 명칭만 다른 뿐이다.
+	#. < DWORD time; > : Double WORD타입으로 WORD두개 크기의 타입이라고 할 수 있다.
+		#. 메세지가 전달되는 시간을 저장하는 공간이다.
+		=> 그런데 64bit윈도우에서 DWORD는 128bit가 아니라 똑같이 64bit이다.
+	#. < POINT pt; > : x와 y좌표를 갖고있는 구조체이다.
+		#. 메세지가 발생할 당시의 마우스 위치가 저장되는 공간이다.
+
+< HIWORD&LOWORD > : 4byte의 메모리 공간을 반으로 쪼개어 16bit씩 나누어 사용한다.
+	#. 그럼 하나의 메모리 공간에 x, y값을 각각 저장할 수 있다.
+	#. DWORD의 앞에 있는 메모리 공간을 HIWORD가
+	#. DWORD의 뒤에 있는 메모리 공간을 LOWORD가 가지고 있다.
+*/
+
 #include <Windows.h>
+#include <sstream>
 
 const wchar_t gClassName[]{ L"MyWindowClass" };
 
@@ -126,7 +263,7 @@ int WINAPI WinMain(
 //		=> 등록된 윈도우 클래스를 통해 윈도우를 찍어낼 수 있다.
 	if (!RegisterClassEx(&wc))
 	{
-		MessageBox(nullptr, L"Failed to register class!", L"Error", MB_OK);
+		MessageBox(nullptr, L"Failed to register Class!", L"Error", MB_OK);
 		return 0;
 	}
 
@@ -135,19 +272,18 @@ int WINAPI WinMain(
 	hWnd = CreateWindowEx(
 		NULL,
 		gClassName,
-		L"Hellow Window",
+		L"Hello Window",
 		WS_OVERLAPPEDWINDOW,
 		0, 0, 640, 480,
 		nullptr, nullptr,
 		hInstance,
-		nullptr
-	);
+		nullptr);
 
 // #2-1. 윈도우 생성 확인
 //		=> CreateWindowEx함수를 통해 윈도우 포인터를 받지 못하였을 경우
 	if (hWnd == nullptr)
 	{
-		MessageBox(nullptr, L"Failed to create Window!", L"Error", MB_OK);
+		MessageBox(nullptr, L"Failed to Create Window!", L"Error", MB_OK);
 		return 0;
 	}
 
@@ -162,8 +298,18 @@ int WINAPI WinMain(
 	MSG msg;
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
+// #. 윈도우에서 실행되는 모든 App에는 '메세지 큐'라는 것이 있다.
+// #. 운영체제는 계속해서 수십개의 메세지를 전달할 것이다. 그럼 이 메세지를 쌓아 놓고 하나씩 처리할 필요가 있다.
+// #. 이 메세지 큐에 메세지를 담아 놓고 하나씩 처리할 것이다.
+// #. 이 작업은 프로그램이 종료될때 까지 계속해서 반복한다.
 		TranslateMessage(&msg);
+// #. 메세지 큐에 메세지가 들어올 때는 운영체제에서 사용하는 메세지가 들어온다.
+// #. 이 메세지를 앱에서 그대로 사용하기에는 규격이 맞지 않는다.
+// #. 따라서 TranslateMessage()함수를 이용해서 번역하는 작업을 먼저 실행한다.
+// #. 이 작업을 통해서 앱에서 사용할 수 있는 포맷으로 메세지를 바꿔준다.
 		DispatchMessage(&msg);
+// #. 앱에는 여러개의 윈도우가 있을 수 있다.
+// #. 포맷이 완료된 메세지를 모든 윈도우들에게 전달하는 일을 해주는 함수가 DispatchMessage()함수다.
 	}
 
 // #3-1. 프로그램 종료
@@ -173,16 +319,34 @@ int WINAPI WinMain(
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+// #. DispatchMessage()함수를 거치고 나면 윈도우에 메세지가 전달된다.
+// #. 윈도우 핸들을 통해 전달된 메세지를 switch문을 통해서 처리한다.
+
 	switch (message)
 	{
+		case WM_KEYDOWN:
+		{
+			std::wostringstream oss;
+			oss << "Virtual Key = " << wParam << std::endl;
+			OutputDebugString(oss.str().c_str());
+// #. wParam에 저장된 가상 키보드의 키 값이 디버그 메시지 창에 출력된다.
+			break;
+		}
+		case WM_LBUTTONDOWN:
+		{
+			std::wostringstream oss;
+			oss << "X : " << LOWORD(lParam) << ", Y : " << HIWORD(lParam);
+			OutputDebugString(oss.str().c_str());
+// #. lParam에 저장된 x, y좌표를 디버그 메시지 창에 출력한다.
+			break;
+		}
 		case WM_CLOSE:
 			DestroyWindow(hWnd);
-			break;
-		case WM_DESTROY:
-			PostQuitMessage(0);
+// #. 관심이 있는 항목은 케이스 레이블을 만들어서 따로 처리해 준다.
 			break;
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
+// #. 일반적인 항목은 기본 처리장치를 통해서 처리해 준다.
 			break;
 	}
 	return 0;
