8c803a326b353dc63758f79a2590d48ddaa09e92
River <hom0906@naver.com>
2023-07-21 16:22:29 +0900

    D2D_정리_애니메이션

--- WindowsMain/WindowMain.cpp
+++ WindowsMain/WindowMain.cpp
@@ -1,18 +1,16 @@
 #include <Windows.h>
-// #. 사전 작업
-#include <d2d1.h>	// Direct2D를 사용하기 위해서는 <d2d1.h>를 포함시켜 줘야 한다.
-#pragma comment(lib, "d2d1.lib")	// 헤더와 함께 라이브러리 포함
+#include <d2d1.h>
+#include <math.h>	// 삼각 함수를 사용하여 그림에 애니메이션을 줘 보자.
+#pragma comment(lib, "d2d1.lib")
 
 const wchar_t gClassName[] = L"MyWindowClass";
 
+void OnPaint(HWND hwnd);
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-// #. Direct2D 오브젝트는 프로그램의 시작/종료까지 관리할 필요가 있다. 예제를 위해서 임시로 전역 변수화 한다.
-//		=> Interface Direct 2D [ID2D]의 약자 형태로 COM을 사용하는 코드에서 자주 등장한다.
 ID2D1Factory* gpD2DFactory{};
 ID2D1HwndRenderTarget* gpRenderTarget{};
-// 간단한 그림 그려보기 1. 브러시 변수 선언
-ID2D1SolidColorBrush* gpBrush{};	// GDI와 마찬가지로 브러시가 준비되어있다. 단색, 그라데이션 브러시를 선언해 보았다.
+ID2D1SolidColorBrush* gpBrush{};
 ID2D1RadialGradientBrush* gpRadialBrush{};
 
 int WINAPI WinMain(
@@ -22,7 +20,6 @@ int WINAPI WinMain(
 	_In_ int nShowCmd
 )
 {
-	// #1. Direct2D Factory 생성 -> 제거를 항상 짝으로 만든다.
 	HRESULT hr = D2D1CreateFactory(
 		D2D1_FACTORY_TYPE_SINGLE_THREADED, &gpD2DFactory
 	);
@@ -73,13 +70,7 @@ int WINAPI WinMain(
 		return 0;
 	}
 
-	// #2. 렌더 타겟 생성 : 그릴 대상을 렌더 타겟이라 부른다. 메모리, 파일, 화면등에 그릴 수 있다.
-	//		=> 메모리에 그리면 그 메모리를 이용하여 특수 효과에 사용할 수 있고
-	//		=> 파일에 그리면 스크린샷이 된다.
-	//		=> 사실 화면에 그린다는 것 또한 그래픽 카드의 메모리에 그리는 것이다.
-	GetClientRect(hwnd, &wr);	// 윈도우 핸들로 부터 윈도우의 클라이언트 영역을 구해 온다.
-
-	// 윈도우 핸들에서 렌더타겟을 생성, 즉 윈도우 내부에 그래픽을 그리게 되는 것
+	GetClientRect(hwnd, &wr);
 	hr = gpD2DFactory->CreateHwndRenderTarget(
 		D2D1::RenderTargetProperties(),
 		D2D1::HwndRenderTargetProperties(
@@ -97,17 +88,13 @@ int WINAPI WinMain(
 		return 0;
 	}
 
-	// 간단한 그림 그려보기 2. 브러시 생성
-		// 색 브러시를 생성한다.
 	hr = gpRenderTarget->CreateSolidColorBrush(
 		D2D1::ColorF(D2D1::ColorF(0x9ACD32, 1.0f)),
 		&gpBrush
 	);
-	// 그라데이션은 시작지점에서 종료지점까지 색상을 자연스럽게 변형시키는 방식이다.
-	// 이를 표현하기 위해서 시작지점, 끝지점에 대한 정보가 필요하며 이를 GradientStop이라 한다.
+
 	ID2D1GradientStopCollection* pGradientStops{};
 	D2D1_GRADIENT_STOP gradientStops[2];
-	// 그라데이션의 분기점을 GradientStop이라 부르며 이를 모아둔 배열을 GradientStopCollection이라 한다.
 	gradientStops[0].color = D2D1::ColorF(D2D1::ColorF::Yellow, 1);
 	gradientStops[0].position = 0.0f;
 	gradientStops[1].color = D2D1::ColorF(D2D1::ColorF::Crimson, 1);
@@ -120,7 +107,6 @@ int WINAPI WinMain(
 		D2D1_EXTEND_MODE_CLAMP,
 		&pGradientStops
 	);
-	// 원형 그라데이션 브러시 생성
 	if (SUCCEEDED(hr))
 	{
 		hr = gpRenderTarget->CreateRadialGradientBrush(
@@ -137,11 +123,30 @@ int WINAPI WinMain(
 	ShowWindow(hwnd, nShowCmd);
 	UpdateWindow(hwnd);
 
+	// #. PeekMessage() : 메시지 큐에 메시지가 존재하면 가져오고 없으면 바로 반환한다.
+	//		=> 따라서 게임과 같이 빠른 처리가 필요하면 이 함수를 사용해야만 한다.
+	//		1. 일단 무한 반복
+	//		2. 메시지가 있나 살펴 본다.
+	//		3. 메시지가 있으면 윈도우 메시지를 처리한다.
+	//			=> 만약 윈도우를 종료하는 메시지라면 루프를 벗어난다.
+	//		4. 메시지가 없으면 게임 루틴을 호출한다.
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (true)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+		}
+		else
+		{
+			OnPaint(hwnd);
+		}
+
 	}
 
 	if (pGradientStops != nullptr)
@@ -162,14 +167,12 @@ int WINAPI WinMain(
 		gpBrush = nullptr;
 	}
 
-	// #4. 해제
 	if (gpRenderTarget != nullptr)
 	{
 		gpRenderTarget->Release();
 		gpRenderTarget = nullptr;
 	}
 
-	// #1. Direct2D Factory 생성 -> 제거를 항상 짝으로 만든다.
 	if (gpD2DFactory != nullptr)
 	{
 		gpD2DFactory->Release();
@@ -185,26 +188,35 @@ void OnPaint(HWND hwnd)
 	PAINTSTRUCT ps;
 	hdc = BeginPaint(hwnd, &ps);
 
-	// #3. 그리기
-		// BeginDraw() / EndDraw() 렌더 타겟에 그릴 준비를 한다. GDI와 비슷한 개념으로 시작과 끝은 각각 지정하면,
-		//		=> gpRenderTarget 내부에서 필요한 작업들을 수행해 준다.
 	gpRenderTarget->BeginDraw();
+	gpRenderTarget->Clear(D2D1::ColorF(0.0f, 0.2f, 0.4f, 1.0f));
 
-	gpRenderTarget->Clear(D2D1::ColorF(0.0f, 0.2f, 0.4f, 1.0f));	// 렌더타겟의 화면을 특정 생삭으로 지움
-
-// 간단한 그림 그려보기 3. 사각형 및 타원 그리기
-	// 속을 채운 사각형, 타원 그리기
+	// #. 투명하게 그리기
+	//		=> 단색 브러시는 색상이나 투명도를 조절할 수 있다.
+	gpBrush->SetOpacity(1.0f);
+	gpBrush->SetColor(D2D1::ColorF(D2D1::ColorF::Aquamarine));
 	gpRenderTarget->FillRectangle(
 		D2D1::Rect(0.0f, 0.0f, 100.0f, 100.0f),
 		gpBrush
 	);
-	gpRenderTarget->FillEllipse(
-		D2D1::Ellipse(D2D1::Point2F(50.0f, 150.0f), 50.0f, 50.0f),
-		gpRadialBrush
+	gpBrush->SetOpacity(0.5f);
+	gpBrush->SetColor(D2D1::ColorF(D2D1::ColorF::LightYellow));
+	gpRenderTarget->FillRectangle(
+		D2D1::Rect(50.0f, 50.0f, 150.0f, 150.0f),
+		gpBrush
 	);
 
-	gpRenderTarget->EndDraw();
+	// #. 그림에 애니메이션 주기
+	//		=> fAngle은 항상 증가해야 하며, 이전에 실행한 값을 가지고 있어야 하므로, 전역변수이거나 정적 변수여야 함
+	static float fAngle = 0.0f;
+	gpRenderTarget->FillEllipse(
+		D2D1::Ellipse(D2D1::Point2F(75.0f + sinf(fAngle) * 25, 150.0f), 50.0f, 50.0f),
+		gpRadialBrush
+	);
+	fAngle += 0.2f;
+	//		=> 위와 같이 실행 하였을 때 윈도우 화면 갱신 메시지 WM_PAINT 때문에 화면을 다시 불러와야 이동한다.
 
+	gpRenderTarget->EndDraw();
 	EndPaint(hwnd, &ps);
 }
 
