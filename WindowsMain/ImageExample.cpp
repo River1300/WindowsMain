a133a1dc19da677a9e26fabfb98c57bbfe5659f2
River <hom0906@naver.com>
2023-07-23 16:57:15 +0900

    D2D_정리_BMP 이미지 뒤집기

--- WindowsMain/ImageExample.cpp
+++ WindowsMain/ImageExample.cpp
@@ -1,5 +1,5 @@
-#include <fstream>	// 파일 읽기
-#include <vector>	// 픽셀 데이터 저장
+#include <fstream>
+#include <vector>
 #include "ImageExample.h"
 
 HRESULT ImageExample::Initialize(HINSTANCE hInstance, LPCWSTR title, UINT width, UINT height)
@@ -23,40 +23,61 @@ void ImageExample::Render()
 
 HRESULT ImageExample::LoadBMP(LPCWSTR filename, ID2D1Bitmap** ppBitmap)
 {
-	// 1. 파일 열기
 	std::ifstream file;
 	file.open("Data/32.bmp", std::ios::binary);
 
 	BITMAPFILEHEADER bmh;
 	BITMAPINFOHEADER bmi;
 
-	// 2. BITMAPFILEHEADER 읽기
 	file.read(reinterpret_cast<char*>(&bmh), sizeof(BITMAPFILEHEADER));
-	// 3. BITMAPINFORHEADER 일기
 	file.read(reinterpret_cast<char*>(&bmi), sizeof(BITMAPINFOHEADER));
 	if (bmh.bfType != 0x4D42) return E_FAIL;
 	if (bmi.biBitCount != 32) return E_FAIL;
 
 	std::vector<char> pPixels(bmi.biSizeImage);
-	// 4. 픽셀로 건너뛰기 -> 바이트 단위로 이동이 가능
-	//		=> bgOffBits에 실제 픽셀에 대한 위치가 저장되어 있다.
 	file.seekg(bmh.bfOffBits);
-	// 5. 비트맵 읽기
-	file.read(&pPixels[0], bmi.biSizeImage);
+	//file.read(&pPixels[0], bmi.biSizeImage); 뒤집어진 이미지를 pitch 단위로 읽어서 올바른 위치(역순)에 저장
 
 	int pitch = bmi.biWidth * (bmi.biBitCount / 8);
+	int index{};
+
+	for (int y = bmi.biHeight - 1; y >= 0; y--)
+	{
+		index = y * pitch;
+		for (int x = 0; x < bmi.biWidth; x++)
+		{
+			char r{}, g{}, b{}, a{};
+
+			file.read(&b, 1);
+			file.read(&g, 1);
+			file.read(&r, 1);
+			file.read(&a, 1);
+
+			if (r == 30 || g == 199 || b == 250) r = g = b = a = 0;
+
+			pPixels[index++] = b;
+			pPixels[index++] = g;
+			pPixels[index++] = r;
+			pPixels[index++] = a;
+		}
+	}
 
 	file.close();
+
+	// 모든 픽셀을 수동으로 읽어서 배경색과 같으면 투명하게 만들어 준다.
+	//		=> 한 바이트씩 전체 비트맵을 읽어 오는 코딩 작성
+	//		=> 읽어온 색상이 배경색과 같으면 RGBA를 모두 0으로 만든다.
+	//		=> LoadBMP()의 비트맵 생성에서 다음 속성을 변경해 준다.
 
 	HRESULT hr = mspRenderTarget->CreateBitmap(
 		D2D1::SizeU(bmi.biWidth, bmi.biHeight),
 		D2D1::BitmapProperties(
 			D2D1::PixelFormat(
-				DXGI_FORMAT_R8G8B8A8_UNORM, D2D1_ALPHA_MODE_IGNORE
+				DXGI_FORMAT_R8G8B8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED
 			)
 		),
 		ppBitmap
-	);	// 비트맵 인터페이스를 생성
+	);
 	ThrowIfFailed(hr);
 
 	(*ppBitmap)->CopyFromMemory(
