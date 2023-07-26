ec16fedd6c2010007844048563df155ae7e6dcab
River <hom0906@naver.com>
2023-07-26 16:05:10 +0900

    D2D_정리_벌래의 회전 + 이동

--- WindowsMain/Bug.cpp
+++ WindowsMain/Bug.cpp
@@ -8,52 +8,45 @@ Bug::Bug(D2DFramework* pFramework) :
 
 	mX = static_cast<float>(rand() % (rct.right - rct.left));
 	mY = static_cast<float>(rand() % (rct.bottom - rct.top));
+
+	mSteps = 0;
+	mRotation = 0.0f;
 }
 
 void Bug::Draw()
-{
-	if (mSteps++ > 5)
+{	// 회전
+	if (mSteps++ > 10)
 	{
 		mSteps = 0;
-		int dir = static_cast<int>(mDirection);
-		int dir_count = static_cast<int>(Direction::COUNT);
-		dir += (1 - rand() % 3);
-		dir = (dir + dir_count) % dir_count;	// 방향 값을 넘어가지 않도록 순환 연산
-
-		mDirection = static_cast<Direction>(dir);
+		mRotation += (1 - rand() % 3) * 45;
 	}
 
-	switch (mDirection)
-	{
-	case Direction::UP:
-		mY--;
-		break;
-	case Direction::UP_RIGHT:
-		mY--;
-		mX++;
-		break;
-	case Direction::RIGHT:
-		mX++;
-		break;
-	case Direction::DOWN_RIGHT:
-		mY++;
-		mX++;
-		break;
-	case Direction::DOWN:
-		mY++;
-		break;
-	case Direction::DOWN_LEFT:
-		mY++;
-		mX--;
-		break;
-	case Direction::LEFT:
-		mX--;
-		break;
-	case Direction::UP_LEFT:
-		mY--;
-		mX--;
-		break;
-	}
+	auto targetPos = UPVECTOR * D2D1::Matrix3x2F::Rotation(mRotation);
+	mX += targetPos.x;
+	mY += targetPos.y;
 
-	Actor::Draw();
+	auto size{ mpBitmap->GetPixelSize() };
+	D2D1_RECT_F rect{
+		0,0,
+		static_cast<float>(0 + size.width),
+		static_cast<float>(0 + size.height)
+	};
+	// 평행 이동 (0, 0) -> (mX, mY)로 이동
+	auto matTranslate = D2D1::Matrix3x2F::Translation(mX, mY);
+	// 비트맵의 회전 중심은 비트맵의 한 가운데를 사용해야 한다.
+	//		=> size.width * 0.5f, ...를 넘겨주어서 가운데를 지정한다.
+	auto matRotation = D2D1::Matrix3x2F::Rotation(
+		mRotation,
+		D2D1_POINT_2F{ size.width * 0.5f, size.height * 0.5f }
+	);
+	// 렌더타겟 전체에 적용
+	//		=> 한 번 지정하면 이후의 모든 그리기에 영향을 준다.
+	mpFramework->GetRenderTarget()->SetTransform(matRotation * matTranslate);
+	//		=> 회전은 원점이 있다는 것이 포인트, 회전 행렬의 원점은 이미지의 중심
+	//		=> 회전을 수행한 후에 이동
+	mpFramework->GetRenderTarget()->DrawBitmap(
+		mpBitmap,
+		rect,
+		mOpacity
+	);
 }
--- WindowsMain/Bug.h
+++ WindowsMain/Bug.h
@@ -1,28 +1,11 @@
 #pragma once
 #include "Actor.h"
 
-// 1. 정해진 위치에서 등장
-// 2. 현재 방향으로 지속적으로 이동
-// 3. 랜덤으로 45도씩 방향 전환
-// 방향이라던가 각각의 객체별로 고유한 정보를 필요 => 파생을 통해서 기능 확장이 필요
-
 class Bug : public Actor
 {
-	enum class Direction
-	{
-		UP,
-		UP_RIGHT,
-		RIGHT,
-		DOWN_RIGHT,
-		DOWN,
-		DOWN_LEFT,
-		LEFT,
-		UP_LEFT,
-		COUNT
-	};
-
-	Direction mDirection{ Direction::UP };
+	const D2D_POINT_2F UPVECTOR{ 0.0f, -1.0f };	// 기준 Vector
 	float mSteps{};
+	float mRotation{};
 
 public:
 	Bug(D2DFramework* pFramework);
