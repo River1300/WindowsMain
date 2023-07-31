1a3ca6de863d3b9deef8f5c5743bdc21033a0f60
River <hom0906@naver.com>
2023-07-31 15:22:11 +0900

    사이즈 변경 옵션

--- WindowsMain/D3DMain.cpp
+++ WindowsMain/D3DMain.cpp
@@ -1,20 +1,5 @@
 #define WIN32_LEAN_AND_MEAN
 
-// #. 과거 Direct Fullscreen Mode
-//	=> 전체화면 그래픽카드 독점 모드
-//	=> 컨텍스트 스위칭( 알트 탭 )을 할 때 VRAM 전체를 사용하고 있던 게임이 지워지고 Window가 그자리를 차지함
-//	=> 작업 전환에 매우 취약해 진다.
-
-// #. 현재 Fullscreen - Windows Mode
-//	=> Window와 Game이 동시에 하드웨어 가속을 받게 됨
-
-// #. 해상도가 변경되면 영향을 받는 리소스
-//		=> SwapChain
-//		=> 렌더타겟( 텍스쳐 ) - 렌더타겟 뷰
-//		=> Depth-Stencill - 뎁스-스텐실 뷰
-//		=> 뷰포트
-//	=> 파이프라인 업데이트
-
 #include <Windows.h>
 #include <wrl/client.h>
 #include <d3d11.h>
@@ -23,11 +8,6 @@
 
 const wchar_t gClassName[]{ L"MyWindowClass" };
 const wchar_t gTitle[]{ L"Direct3D Example" };
-
-// #. 윈도우의 창이 커지면 해상도 또한 커져야 한다.
-//		=> WM_ENTERSIZEMOVE
-//		=> WM_SIZE
-//		=> WM_EXITSIZEMOVE
 int gScreenWidth{ 800 };
 int gScreenHeight{ 600 };
 
@@ -49,7 +29,6 @@ void DestroyD3D();
 void Render();
 void OnResize();
 
-// 현재 윈도우 상태의 상태를 체크할 플래그
 bool gMinimized{ false };
 bool gMaximized{ false };
 bool gResizing{ false };
@@ -72,7 +51,9 @@ int WINAPI WinMain(
 	wc.lpszClassName = gClassName;
 	wc.hInstance = gInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	// 윈도우가 기본 윈도우를 그리고 이후에 게임을 그리는데 이는 두 번의 그리기 작업을 실행하는 것으로 낭비다.
+	//		=> 그러므로 배경 그리기를 지운다.
+	wc.hbrBackground = nullptr;
 	wc.lpfnWndProc = WindowProc;
 	wc.cbSize = sizeof(WNDCLASSEX);
 
@@ -144,12 +125,25 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 {
 	switch (message)
 	{
-		// 1. 사이즈 변경 중
+	case WM_PAINT:
+		// 사이즈를 변경 중일 때는 Render로 그려주고
+		if (gResizing)
+		{
+			Render();
+		}
+		else
+		{
+			// 변경 중이 아닐 때는 윈도우 기본 그리기를 호출한다.
+			PAINTSTRUCT ps;
+			BeginPaint(hWnd, &ps);
+			EndPaint(hWnd, &ps);
+		}
+		break;
+
 	case WM_ENTERSIZEMOVE:
 		gResizing = true;
 		break;
 
-		// 3. 사이즈 변경 처리
 	case WM_SIZE:
 		gScreenWidth = LOWORD(lParam);
 		gScreenHeight = HIWORD(lParam);
@@ -189,10 +183,8 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 				}
 			}
 		}
-
 		break;
 
-		// 2. 사이즈 변경 완료
 	case WM_EXITSIZEMOVE:
 		gResizing = false;
 		OnResize();
@@ -228,10 +220,8 @@ void InitD3D()
 	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
 	scd.BufferCount = 1;
 	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	// 버퍼의 가로/세로를 지정
 	scd.BufferDesc.Width = gScreenWidth;
 	scd.BufferDesc.Height = gScreenHeight;
-	// 윈도우 창의 모드를 전환할 수 있는 옵션
 	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	scd.OutputWindow = gHwnd;
@@ -258,7 +248,6 @@ void InitD3D()
 
 void DestroyD3D()
 {
-	// 종료하기 전에 현재 윈도우가 창모드인지 확인하여 창모드로 돌려준다.
 	gspSwapChain->SetFullscreenState(FALSE, nullptr);
 
 	gspDepthStencil.Reset();
@@ -293,11 +282,6 @@ void Render()
 
 void OnResize()
 {
-	// #. 그래픽카드 정리
-	// SwapChain의 버퍼 크기 수정
-	//		=> DirectX는 GPU에서 사용되는 리소스를 다룬다.
-	//		=> CPU가 GPU에게 명령을 내리면 VRAM에서 차례로 명령을 수행한다.
-	//		=> CPU와 GPU의 명령 처리 속도가 서로 동기화되지 않는다면 화면의 크기가 바뀌어도 이전 크기의 버퍼를 사용할 수 있다.
 	ID3D11RenderTargetView* nullViews[] = { nullptr };
 	gspDeviceContext->
 		OMSetRenderTargets(_countof(nullViews), nullViews, nullptr);
@@ -307,7 +291,7 @@ void OnResize()
 	gspRenderTarget.Reset();
 	gspDepthStencil.Reset();
 
-	gspDeviceContext->Flush();	// 쌓여있는 CPU의 명령을 전부 처리하고 오라는 동기화 함수
+	gspDeviceContext->Flush();
 
 	gspSwapChain->ResizeBuffers(
 		0,
